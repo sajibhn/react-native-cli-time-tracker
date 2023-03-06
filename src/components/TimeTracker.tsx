6d60ca22cb59567987f04d37cd79d9e0fc1ca212
Sajib Hossain <sajibhossain1185@gmail.com>
2023-03-06 16:40:21 +0600

    added firebase firestore

--- src/components/TimeTracker.tsx
+++ src/components/TimeTracker.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firestore from '@react-native-firebase/firestore';
+
+interface DateData {
+  date: string;
+  startTime: number;
+  endTime: number;
+  id: string;
+}
 const TimeTracker = () => {
+  const [dates, setDates] = useState<DateData[]>([]);
+  const ref = firestore().collection('time-tracker');
+
+  useEffect(() => {
+    ref.onSnapshot(querySnapshot => {
+      const datesArr: DateData[] = [];
+      querySnapshot.forEach(doc => {
+        datesArr.push({ ...doc.data(), id: doc.id } as DateData);
+      });
+      setDates(datesArr);
+    });
+  }, []);
+
+  console.log(dates);
+
   return (
     <View>
-      <Text style={styles.text}>Timeracker</Text>
+      <Text style={styles.text}>good what happed to hot reload ok it is working fg</Text>
     </View>
   );
 };
