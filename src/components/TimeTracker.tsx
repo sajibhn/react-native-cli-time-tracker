3e24295df59aa092278fa9b28212c8078e1b3fbc
Sajib Hossain <sajibhossain1185@gmail.com>
2023-03-06 12:04:59 +0600

    added google fonts

--- src/components/TimeTracker.tsx
+++ src/components/TimeTracker.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const TimeTracker = () => {
   return (
     <View>
-      <Text>TimeTracker</Text>
+      <Text style={styles.text}>Timeracker</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25,
+    fontFamily: 'Inter-Regular',
+  },
+});
 
 export default TimeTracker;
