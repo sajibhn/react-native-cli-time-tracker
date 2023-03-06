30b7b8de00f4afa98d27f9db55374443605f38bc
Sajib Hossain <sajibhossain1185@gmail.com>
2023-03-06 11:58:20 +0600

    added svg components

---
+++ src/components/TimeTracker.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const TimeTracker = () => {
+  return (
+    <View>
+      <Text>TimeTracker</Text>
+    </View>
+  );
+};
+
+export default TimeTracker;
